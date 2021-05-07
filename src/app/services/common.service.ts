ae23f3e14d7c45f8c8e73f9981dcda8b7f3624ce
sulochana.dain@shauryatechnosoft.com <78153674+sulochana-dain@users.noreply.github.com>
2021-05-07 16:33:11 +0530

    Update common.service.ts

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -10,8 +10,8 @@ export class CommonService {
   constructor(private http:HttpClient) { }
 
   getData(){
-    return this.http.get('http://awsapi.mahamining.com/mineral-mapping/licensees/GetAllLicensee?LicenseeType=&isCompleted=2&pageno=1&pagesize=10');
+    //return this.http.get('http://awsapi.mahamining.com/mineral-mapping/licensees/GetAllLicensee?LicenseeType=&isCompleted=2&pageno=1&pagesize=10');
 
-     //return this.http.get('https://jsonplaceholder.typicode.com/posts');
+     return this.http.get('https://jsonplaceholder.typicode.com/posts');
   }
 }
